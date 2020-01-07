14a386b45b21fe497955be722618a4ae890d1136
Tatsuhiko <okihiutat.iutim@gmail.com>
2020-01-07 21:43:57 +0900

    trasfer from previous

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <div>
+    <div>test</div>
+  </div>
+)
+
+export default IndexPage
