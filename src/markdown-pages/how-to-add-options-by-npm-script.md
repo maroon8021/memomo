b5398389bb4e67fb5b6f7d4c6f1c2939bcfc5d28
mitsui_t <okihiutat.iutim@gmail.com>
2020-01-10 13:30:58 +0900

    add logic to render markdown pages

---
+++ src/markdown-pages/how-to-add-options-by-npm-script.md
@@ -0,0 +1,16 @@
+---
+path: "/article/how-to-add-options-by-npm-script"
+date: "2019-07-04"
+---
+
+# どうやったら npm-script に引数を渡せるのか
+
+```
+npm run hoge -- hogehoge
+```
+
+といったかんじで、 `--` 以下に文字入れると、 **後ろにくっつく** らしい
+
+[https://stackoverflow.com/questions/44459774/how-to-push-to-github-with-custom-commit-message-with-npm-scripts](https://stackoverflow.com/questions/44459774/how-to-push-to-github-with-custom-commit-message-with-npm-scripts)
+
+[https://qiita.com/qrusadorz/items/db042f65be95f34d6271](https://qiita.com/qrusadorz/items/db042f65be95f34d6271)
---
+++ src/templates/article-base.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "@s/components/common/layout"
+import Header from "@s/components/common/header"
+
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds our post data
+  const { html } = markdownRemark
+  return (
+    <Layout>
+      <Header />
+      <div className="section article">
+        <div dangerouslySetInnerHTML={{ __html: html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
