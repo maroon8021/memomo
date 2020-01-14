3ab7c94e28ec5275b21482a9fd2f0874d27768b4
maroon8021 <maroon8021@gmail.com>
2020-01-14 16:47:57 +0900

    Add file name validation to make-md.sh & remove static/data.json

--- make-md.sh
+++ make-md.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 cd ./src/markdown-pages/
+
+# https://linuxfan.info/post-1739
+if [[ -e $1.md ]]; then #同名ファイルが存在する場合
+  echo 'The file name "'$1'" already exists'
+  echo 'Please use other name for md title'
+  exit
+fi
+
 touch $1.md
 
 echo 'The file named as ' $1.md 'is created'
