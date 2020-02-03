df506986585d4a950cc111663c66a6c53d0bc777
maroon8021 <maroon8021@gmail.com>
2020-02-03 17:41:22 +0900

    add validation of arguments

--- make-md.sh
+++ make-md.sh
@@ -7,6 +7,11 @@ if [[ -e $1.md ]]; then #同名ファイルが存在する場合
   echo 'The file name "'$1'" already exists'
   echo 'Please use other name for md title'
   exit
+
+elif [ $# = 0 ]; then #引数チェック https://hacknote.jp/archives/33122/
+  echo 'No file name(an argument)'
+  echo 'Please set one argument as md title'
+  exit
 fi
 
 touch $1.md
